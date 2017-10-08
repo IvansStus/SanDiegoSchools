d912a01f4ff410a2662e559db898a357fbeb9700
mGalarnyk <mgalarny@gmail.com>
2017-10-07 19:09:11 -0700

    Added webscraper with ability to take data from great school with ethnicity

--- WebScraping/greatschools/greatschools/spiders/great_schools_scrape.py
+++ WebScraping/greatschools/greatschools/spiders/great_schools_scrape.py
@@ -3,7 +3,11 @@ from greatschools.items import GreatschoolsItem
 from datetime import datetime
 import re
 import json
-
+from flatten_json import flatten_json
+from pandas.io.json import json_normalize
+import pandas as pd
+import numpy as np
+from urlparse import urlparse
 
 class Fundrazr(scrapy.Spider):
 	name = "my_scraper"
@@ -55,18 +59,75 @@ class Fundrazr(scrapy.Spider):
 		item['studentsNumber'] = toDistributeList[3]
 		item['types'] = toDistributeList[5]
 
-
 		# The following section gets Race/ethnicity, Low-income students, students with disabilities
 		lookslikeJson = response.xpath("//div[contains(@data-component-name, 'SchoolProfileComponent')]/@data-props").extract()
 
-		jsonDict = {}
-		for oneJson in lookslikeJson:
-			newJson = json.loads(oneJson) 
+		newJson = json.loads(lookslikeJson[0])
+
+		df = json_normalize(flatten_json(newJson))
+
+		# Replacing Really Long Column Names
+		df.columns = df.columns.str.replace('California Assessment of Student Performance and Progress \(CAASPP\)', 'CAASPP')
+
+		# Science has a different standardized Test than English and Math, but calling them same for coding
+		df.columns = df.columns.str.replace('California Standards Tests', 'CAASPP')
+
+		# #creates a new dataframe that's empty
+		allSubjectDF = pd.DataFrame(columns = ['Subject', 'Ethnicity', 'schoolScore','stateScore']) 
+
+				# This is the start of column names this accounts for subjects 
+		base_list = ['data_0_data_1', 'data_0_data_2', 'data_0_data_3']
+
+		for base in base_list: 
+		             
+		    # Get School subject this will be merged in last to dataframe
+		    subject = base + '_title'
+		    subjectCol = df.columns[df.columns.str.contains(pat = subject)]
+		    subjectToAdd = df[subjectCol.values].values[0]
+		             
+		    # Score
+		    score = base + '_values_CAASPP_[0-9]_score'
+		    scoreCol = df.columns[df.columns.str.contains(pat = score)]
+		    scoreDf = pd.DataFrame(data = np.transpose(df[scoreCol.values].values), columns = ['schoolScore'])
+		    
+		    # State Score Comparison
+		    stateScore = base + '_values_CAASPP_[0-9]_state_average$'
+		    stateCol = df.columns[df.columns.str.contains(pat = stateScore)]
+		    stateDf = pd.DataFrame(data = np.transpose(df[stateCol.values].values), columns = ['stateScore'])
+		    
+		    # Ethnicity of the Students
+		    ethnicity = base + '_values_CAASPP_[0-9]_breakdown'
+		    ethnicityCol = df.columns[df.columns.str.contains(pat = ethnicity)]
+		    ethnicityDf = pd.DataFrame(data = np.transpose(df[ethnicityCol.values].values), columns = ['Ethnicity'])
+		         
+		    # Concatenating Dataframes 
+		    ethSchoolStateDf = pd.concat([ethnicityDf, scoreDf, stateDf], axis = 1)
+		    
+		    ethSchoolStateDf['Subject'] = subjectToAdd[0]
+		    
+		    allSubjectDF = pd.concat([allSubjectDF, ethSchoolStateDf], axis = 0,ignore_index=True) # ignoring index is optional
+		 
+		allSubjectDF['gsID'] = response.url.rsplit('/', 2)[-2].split("-")[0]
+
+		allSubjectDF.to_csv('greatSchoolsTestScore.csv', mode = 'a', header = False, index = False)
+		     
+
+		# Loop to merge in all subjects
+
+		#jsonDict = {}
+		#for oneJson in lookslikeJson:
+			#newJson = json.loads(oneJson) 
+
+			# json_normalize(flatten_json(json.loads(lookslikeJson[0])))
+			#newJson = flatten_json(newJson)
+
+			#newJson
+			# newJson.to_csv('test.csv', encoding = 'utf-8')
 
 			# This is to make it easier to know what nested JSON are
-			jsonDict[newJson['anchor']] = newJson
+			#jsonDict[newJson['anchor']] = newJson
 
-		item['lowIncomeRaceDisbilityJson'] = jsonDict
+		#item['lowIncomeRaceDisbilityJson'] = jsonDict
 
 		yield item
 
